79a7c4355d28aeff8d2b7cb73671df607b19431b
Sudhakaran_roni <32321500+Sudhakaran7@users.noreply.github.com>
2020-08-27 18:47:44 +0700

    Create analyze.py

---
+++ analyze.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+from itertools import combinations
+from typing import List
+class Solution:
+    def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
+        users = defaultdict(list)
+        for i in range(0, len(username)):
+            users[username[i]].append((timestamp[i], website[i]))
+        three_sequence = defaultdict(int)
+        for u in users.keys():
+            webs = [web for time, web in sorted(users[u])]
+            combs = combinations(webs, 3)
+            user_set = {c for c in combs}
+            for item in user_set:
+                three_sequence[item] += 1
+        ans = sorted(three_sequence.items(), key=lambda x: (-x[1], x[0][0], x[0][1], x[0][2]))
+        return list(ans[0][0])
+val=Solution()
+N=int(input())
+username=list(map(str,input().split()))
+timestamp=list(map(int,input().split()))
+website=list(map(str,input().split()))
+print(*val.mostVisitedPattern(username,timestamp,website))
